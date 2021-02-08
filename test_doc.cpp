d7248073dc8ff8c086adc12ade76e89c761d9350
Robert Argasinski <ra536@njit.edu>
2021-02-08 13:31:21 -0500

    Insert into A until max

--- test_doc.cpp
+++ test_doc.cpp
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <string>
 #include <iostream>
+#include <string.h>
 using namespace std;
 
 int N = 11;
 char T[11] = "ask";
 int nextIdxA = 0;
+int sizeA = 100;
 char A[100] = "jeeves";
 
 int simpleHash(char *k){
@@ -18,14 +20,52 @@ int simpleHash(char *k){
 	return (sum-2) % N;
 }
 
+int appendToArray(char *word, int wordSize, char *originalArray, int arraySize, int lastIdx){
+	// add check on size
+	int i=0;
+	while(word[i] != '\0'){
+		originalArray[lastIdx+i]=word[i];
+		i++;
+	}
+	originalArray[lastIdx+i]='\0';
+	printf("%s\n", &originalArray[lastIdx]);
+	return lastIdx+i+1; // add the 0B
+}
+
+char *resizeArray(char *originalArray, char *newArray, int oldSize, int newSize){
+	strcpy(newArray, originalArray);
+	return newArray;
+}
+
 int main(){
 	T[N-1] = '\0';
 	A[99] = '\0';
 	printf("Hello world\n");
 	char input[] = "alex";
 	cout << to_string(simpleHash(input)) << endl;
-	printf("%s", T);
-	printf("%s", A);
+	printf("%s\n", T);
+	printf("%s\n", A);
+	cout << (int *) &A[0] << endl;
+	cout << (int *) &A[1] << endl;
+	cout << (int *) &A[99] << endl;
+	char boy[4] = "hi!";
+	char girl[20];
+	strcpy(girl, boy);
+	strcat(girl, " how are you?");
+	printf("%s", girl);
+
+	char first[20];
+	char inp[4] = "the";
+	int last=0;
+	while (20-last >= 4){
+		last = appendToArray(inp, 4, first, 20, last);
+	}
+	for(int i=0; i<20; ++i){
+		if(first[i]=='\0')
+			printf("@,");
+		else 
+			printf("%c,",first[i]);
+	}
 	return 0;
 }
 
