b98a15b8a4e436f5a717bd9338e99172f8aba1ae
Robert Argasinski <ra536@njit.edu>
2021-02-09 10:47:55 -0500

    Resizing works for hash table, by using relative addresses

--- test_doc.cpp
+++ test_doc.cpp
@@ -5,10 +5,10 @@
 using namespace std;
 
 int N = 11;
-char T[11] = "ask";
-int nextIdxA = 0;
-int sizeA = 100;
-char A[100] = "jeeves";
+// char T[11] = "ask";
+// int nextIdxA = 0;
+// int sizeA = 100;
+// char A[100] = "jeeves";
 
 int simpleHash(char *k){
 	int idx=0;
@@ -20,7 +20,16 @@ int simpleHash(char *k){
 	return (sum-2) % N;
 }
 
-int appendToArray(char *word, int wordSize, char *originalArray, int arraySize, int lastIdx){
+int needGrow(char *word, int wordSize, char *array, int arraySize, int lastIdx){
+	if(wordSize+1 <= arraySize-lastIdx){
+		return 1; // FALSE
+	}
+	else {
+		return 0; // TRUE
+	}
+}
+
+int appendToArray(char *word, int wordSize, char *originalArray, int arraySize, int lastIdx, char *table[], int tableIdx){
 	// add check on size
 	int i=0;
 	while(word[i] != '\0'){
@@ -29,15 +38,30 @@ int appendToArray(char *word, int wordSize, char *originalArray, int arraySize, 
 	}
 	originalArray[lastIdx+i]='\0';
 	// printf("%p\n", (int *) &originalArray[lastIdx]);
+	table[tableIdx]=&originalArray[lastIdx];
 	return lastIdx+i+1; // add the 0B
 }
 
-char *resizeArray(char *originalArray, char *newArray, int oldSize, int newSize){
+char *resizeArray(char *originalArray, char *newArray, int oldSize, int newSize, char *hashTableOld[], char *hashTableNew[], int tableSize){
 	for(int i=0; i<oldSize; i++){
 		newArray[i] = originalArray[i];
 	}
+	for(int j=0; j<tableSize; j++){
+		printf("PTR: %ld\n", hashTableOld[j]-&originalArray[0]);
+		hashTableNew[j] = &newArray[0] + (hashTableOld[j]-&originalArray[0]) ;
+	}
 	//strcpy(newArray, originalArray);
 	return newArray;
+}
+
+void addToTable(char *table[], int tableIdx, char *array, int lastIdx){
+	table[tableIdx] = &array[lastIdx];	
+}
+
+void printHashContents(char *table[], int size){
+	for(int i=0; i < size; i++){
+		printf("%p\n", table[i]);
+	}
 }
 
 void printArrayContentsAddress(char *array, int size){
@@ -74,30 +98,53 @@ int main(){
 	cout << to_string(simpleHash(input)) << endl;
 	cout << endl;
 
+
 	char first[20];
+	printf("Location of first: %p\n", &first);
 	char inp[4] = "the";
+	printf("Location of string 'the': %p\n", &inp);
 	int last=0;
-	while (20-last >= 4){
-		last = appendToArray(inp, 4, first, 20, last);
+	printf("Location of int 'last': %p\n", &last);
+	
+	char *hashTableFirst[5];
+	int i=0;
+	while (20-last >= 4){ // and wordsize does not exceed bounds (see needGrow)
+		last = appendToArray(inp, 4, first, 20, last, hashTableFirst, i);
+		i++;
 	}
 	char second[50];
 	char inp2[6] = "other";
-
-	resizeArray(first, second, 20, 50);
+	printf("Location of second: %p\n", &second);
+	printf("Location of string 'other': %p\n", &inp2);
 	
+	char *hashTableSecond[10];
+
+	resizeArray(first, second, 20, 50, hashTableFirst, hashTableSecond, 5);
+	
+	//i = 0;
 	while (50-last >= 6){
-		last = appendToArray(inp2, 6, second, 50, last);
+		//addToTable(hashTable, i, second, last);
+		last = appendToArray(inp2, 6, second, 50, last, hashTableSecond, i);
+		i++;
 	}
 
 	printf("--FIRST-----------\n");
 	printArrayContentsNormal(first, 20);
 	printArrayContentsAddress(first, 20);
-
+	
 	cout << endl;
 	printf("--SECOND----------\n");
 	printArrayContentsNormal(second, 50);
 	printArrayContentsAddress(second, 50);
 	
+	cout << endl;
+	printf("--Table for FIRST----------\n");
+	printHashContents(hashTableFirst, 5);
+	
+	cout << endl;
+	printf("--Table for SECOND----------\n");
+	printHashContents(hashTableSecond, 10);
+
 	cout << endl;
 	return 0;
 }
